cf0c41d36e738e9d276466e9ee50477db9fd0a26
Mr. Packman <99031872+PackmanDude@users.noreply.github.com>
2022-08-01 13:01:11 +0300

    Update redirect.js

--- source-code/redirect.js
+++ source-code/redirect.js
@@ -1,4 +1,1 @@
-if (window.location.href==="https://packmandude.github.io/packman-site/source-code/")
-{
-    window.location.href = "https://github.com/PackmanDude/packman-site";
-}
+window.location.href = "https://github.com/PackmanDude/packman-site";
